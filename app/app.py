4d06a5cd098be6a47daeed04743cb2dbfbada18c
Elia Manara <elia.manara@aiknow.io>
2020-12-04 12:06:21 +0100

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,26 @@
+import time
+import os
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'))
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    mypassword = os.environ.get('PASSWORD')
+    return 'Hello World! I have been seen {} times.\nMy password: {}'.format(count, mypassword)
+
